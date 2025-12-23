1cb6a498a78b6636ac013603f2231de76f0392db
65143332 watcharin <famerock51@gmail.com>
2025-12-23 10:29:50 +0700

    fix facility

--- app/Config/Session.php
+++ app/Config/Session.php
@@ -57,7 +57,7 @@ class Session extends BaseConfig
      *
      * IMPORTANT: You are REQUIRED to set a valid save path!
      */
-    public string $savePath = 'E:\\work\\mini_project_work_experience\\Github\\stadium_marketplace\\writable\\session' . 'session';
+    public string $savePath = WRITEPATH . 'session';
 
     /**
      * --------------------------------------------------------------------------
